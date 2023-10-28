b46a4cb2cde2383db4b9f7cec25287e33b324d55
José André <98027989+joseandre25@users.noreply.github.com>
2023-10-28 15:09:13 -0300

    Update DQL.sql

--- sources/DQL.sql
+++ sources/DQL.sql
@@ -26,3 +26,34 @@ SELECT AR.nome
 FROM Armadura AR
 JOIN GuardaArmadura GAR ON AR.IdArmadura = GAR.Armadura
 JOIN Inventario INV ON INV.IdInventario = GAR.Inventario;
+
+-- Armaduras que tem raridade maior ou igual a 5
+SELECT *
+FROM Armadura
+WHERE Raridade >= 5;
+
+-- Todas as lojas que vendem ferramentas de cura (pode ser reutilizado pra outras fuinções):
+SELECT L.Nome AS NomeLoja, F.Nome AS NomeFerramenta
+FROM Loja AS L
+JOIN VendeFerramenta AS VF ON L.IdLoja = VF.Loja
+JOIN Ferramenta AS F ON VF.Ferramenta = F.IdFerramenta
+WHERE F.Funcao = 'Cura';
+
+-- Listar todos os amuletos guardados por um jogador específico:
+SELECT A.Nome AS NomeAmuleto
+FROM Amuleto AS A
+JOIN GuardaAmuleto AS GA ON A.IdAmuleto = GA.Amuleto
+WHERE GA.Inventario = 1; -- Aqui a gente substitui pelo id do player desejado.
+
+-- Calcular o custo total de todas as ferramentas vendidas por uma loja:
+SELECT L.Nome AS NomeLoja, SUM(F.CustoCompra) AS CustoTotal
+FROM Loja AS L
+JOIN VendeFerramenta AS VF ON L.IdLoja = VF.Loja
+JOIN Ferramenta AS F ON VF.Ferramenta = F.IdFerramenta
+GROUP BY L.Nome;
+
+-- Listar as habilidades associadas a uma armadura específica:
+SELECT H.Nome AS NomeHabilidade
+FROM Habilidade AS H
+JOIN HabilidadeArmadura AS HA ON H.IdHabilidade = HA.Habilidade
+WHERE HA.Armadura = 1; -- Aqui a gente substitui pelo id da armadura desejada.
