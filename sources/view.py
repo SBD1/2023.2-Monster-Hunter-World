3c06e92264fd3ac3ee90de8053e3a8114ded1869
joseandre25 <andrerabelo2503@gmail.com>
2023-12-01 22:21:23 -0300

    :pencil: alteração no docker-compose.yml

--- sources/view.py
+++ sources/view.py
@@ -35,3 +35,40 @@ def pageCriarPersonagem():
         ]
     }
     return render_template('index.html',page=page)
+
+def pageTutorial():
+    page = {
+        "name": "Área de Encontro",
+        "background": "tutorialBackground.png",
+        "content": [
+            {
+                "type": "text",
+                "text": "Bem-vindo à Área de Encontro, --pegar da controller o nome!" 
+            },
+            {
+                "type": "text",
+                "text": "Passo 1: Esta é a sua jornada. Seu personagem foi criado com sucesso!"
+            },
+            {
+                "type": "text",
+                "text": "Passo 2: Agora, você aprenderá as mecânicas do jogo."
+            },
+            {
+                "type": "text",
+                "text": "Passo 3: Explore o mundo, lute contra monstros e complete missões emocionantes."
+            },
+            {
+                "type": "text",
+                "text": "Passo 4: Ao longo do caminho, você encontrará desafios e recompensas."
+            },
+            {
+                "type": "button",
+                "text": "Iniciar Jogo",
+                "action": "iniciarJogo"
+            },
+        ]
+    }
+    return render_template('index.html', page=page)
+
+
+
